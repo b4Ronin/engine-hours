94c3ad8a237d549e5ae26a689ef16cdbcf31c6b8
b4Ronin <b4ronin@yahoo.com>
2026-04-02 14:41:44 -0500

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME="engine-hours-a5-v2";
-self.addEventListener("install",e=>{
- e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(["./","./index.html","./app.js","./style.css"])));
- self.skipWaiting();
-});
-self.addEventListener("fetch",e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
